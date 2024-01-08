d00dc15203074be02b7af8d78643b6b505e48b50
vitoraugusto17 <vitoraugustoeleuterio1@gmail.com>
2024-01-08 18:28:00 -0300

    Feat: add the TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
 import symbolOn from './assets/pictures/symbol-on.png';
 import symbolOff from './assets/pictures/symbol-off.png';
 
 
 export default function App() {
 
-  const isActive = true;
-  return (
-    <View style={  isActive ? styles.containerOn : styles.containerOff}>
-        <Image source={isActive ? symbolOn : symbolOff}/>
+  let isActive = true;
 
+  function handleSingle(){
+    isActive = !isActive;
+
+  }
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSingle}>
+           <Image source={isActive ? symbolOn : symbolOff}/>
+      </TouchableOpacity>
     </View>
   );
 }
